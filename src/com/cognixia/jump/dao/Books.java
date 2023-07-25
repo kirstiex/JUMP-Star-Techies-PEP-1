81a2c53600f53d25cc4e6c59c922e409f020bd44
kirstiex <ikirstiew@gmail.com>
2023-07-25 12:56:00 -0500

    DAO files

---
+++ src/com/cognixia/jump/dao/Books.java
@@ -0,0 +1,21 @@
+package com.cognixia.jump.dao;
+/*
+	DAO MODEL CLASS
+	---------------------------
+	1.) Make sure the following data is being stored:
+		- id
+		- username
+		- password
+		- status
+		
+	2.) Add a constructor that accepts all the attributes as arguments
+	
+	3.) Have getters/setters for each attribute
+	
+	4.) toString() method to help with testing
+*/
+
+
+public class Books {
+
+}
